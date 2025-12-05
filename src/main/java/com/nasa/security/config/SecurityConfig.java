57f2719868b3a2ba4b9994772f19d1be36b9e13f
Sanjay Kumar <sanjay.kumar@cscs.io>
2025-12-05 21:01:29 +0530

    Added the securiyt bean

---
+++ src/main/java/com/nasa/security/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.nasa.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+
+}
